48716828a96e5e58e5de5615263f6f2667593233
Anna Jarocińska <anna.jarocinska5@gmail.com>
2019-10-03 12:08:38 +0200

    Amendments in BMI Informations.js

--- BMI/Informations.js
+++ BMI/Informations.js
@@ -19,8 +19,5 @@ class Informations {
         if (this.weight <= 0 || this.height <= 0) {
             alert("Podana wartość jest za niska")
         }
-        if (this.weight >= this.height) {
-            alert("Podano nieprawidłowe wartości")
-        }
     }
 }
